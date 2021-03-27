5225b9749b92fded75fa5da36413005586925ac1
Mattia Franchetto <mt.franchetto@gmail.com>
2021-03-27 19:53:57 +0100

    Bind todos list

--- CasavoAssessment/src/UserDetail.tsx
+++ CasavoAssessment/src/UserDetail.tsx
@@ -1,15 +1,41 @@
-import React, { useCallback } from 'react';
-import { Button, Linking } from 'react-native';
-import { useAppSelector } from './core/hooks';
+import React, { useCallback, useEffect } from 'react';
+import { Button, Linking, ListRenderItemInfo, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import { useAppDispatch, useAppSelector } from './core/hooks';
+import { todosStyles } from './styles';
+import { fetchTodosForUser } from './todos';
+import { Todo } from './types';
+
+const TodoRow = ({ completed, id, title, userId }: Todo) => {
+  return (
+    <Text style={todosStyles.title}>{title}</Text>
+  )
+}
 
 export const UserDetail = () => {
+  const dispatch = useAppDispatch();
   const user = useAppSelector(state => {
     const { list, selectedUserId } = state.users;
     return list.find(u => u.id === selectedUserId);
   });
+  const { list: todos } = useAppSelector(state => state.todos);
+  useEffect(() => {
+    dispatch(fetchTodosForUser(user!.id));
+  }, [user]);
+
   const callUser = useCallback(() => Linking.openURL(`tel:${user!.phoneNumber}`), [user]);
-  
+  const renderRow = useCallback(({ item }: ListRenderItemInfo<Todo>) => (
+    <TodoRow {...item} />
+  ), []);
+
   return (
-    <Button title="Call" onPress={callUser} />
+    <FlatList
+      ListHeaderComponent={() => ( // this should be extracted for proper memoization
+        <Button title="Call" onPress={callUser} />
+      )}
+      keyExtractor={todo => todo.id}
+      data={todos}
+      renderItem={renderRow}
+    />
   )
 };
